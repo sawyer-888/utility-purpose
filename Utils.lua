5ae1189444c6460b8c51f07f6a8b28009abf1439
sawyer888 <148013118+sawyer-888@users.noreply.github.com>
2025-03-13 23:33:42 -0400

    Update Utils.lua

--- Utils.lua
+++ Utils.lua
@@ -5,5 +5,8 @@ Utils.Vector = require(script.VectorUtils)
 Utils.Instance = require(script.InstanceUtils)
 Utils.Table = require(script.TableUtils)
 Utils.Network = require(script.NetworkUtils)
+Utils.UI = require(script.UIUtils)
+Utils.Pathfinding = require(script.PathfindingUtils)
+Utils.Debug = require(script.DebugUtils)
 
 return Utils

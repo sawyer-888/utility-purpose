db151acb8b8248b067e4df06b3ee8c35f2b37fb6
sawyer888 <148013118+sawyer-888@users.noreply.github.com>
2025-03-13 23:32:05 -0400

    Create VectorUtils.lua

---
+++ VectorUtils.lua
@@ -0,0 +1,12 @@
+local VectorUtils = {}
+
+function VectorUtils.RotateVector(vec, axis, angle)
+    local q = CFrame.fromAxisAngle(axis, math.rad(angle))
+    return (q * vec) - q.Position
+end
+
+function VectorUtils.AngleBetween(v1, v2)
+    return math.deg(math.acos(v1.Unit:Dot(v2.Unit)))
+end
+
+return VectorUtils

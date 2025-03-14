5df36ed567dffc6d5f2a5249a169ac693294d4ec
sawyer888 <148013118+sawyer-888@users.noreply.github.com>
2025-03-13 23:34:00 -0400

    Update VectorUtils.lua

--- VectorUtils.lua
+++ VectorUtils.lua
@@ -9,4 +9,8 @@ function VectorUtils.AngleBetween(v1, v2)
     return math.deg(math.acos(v1.Unit:Dot(v2.Unit)))
 end
 
+function VectorUtils.ProjectOntoPlane(v, normal)
+    return v - normal * v:Dot(normal)
+end
+
 return VectorUtils
